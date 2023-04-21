268abc4f7998831196025217f4e2593e66fb2138
weltonbatist <weltonbatist@gmail.com>
2023-04-21 01:48:39 -0300

    Add API Summary

---
+++ src/CashFlow/CashFlow.CD.Api/configurations/DependencyInjectionExtensions.cs
@@ -0,0 +1,24 @@
+﻿using CashFlow.CD.API.Service;
+using CashFlow.CD.Domain.domain;
+using CashFlow.CD.Domain.repository;
+using CashFlow.CD.Infra.repository;
+using CashFlow.RC.Domain.domain;
+
+namespace CashFlow.CD.Api.configurations
+{
+    public static class DependencyInjectionExtensions
+    {
+        public static void AddFinancialReportService(this IServiceCollection services)
+        {
+            services.AddScoped<IFinancialReportService,FinancialReportService>();
+        }
+
+        public static IServiceCollection AddFinancialReportConsolidateDaily(this IServiceCollection services, IConfiguration configuration)
+        {
+            string connectionString = configuration.GetConnectionString("MongoDB");
+            services.AddSingleton<IEntityRepository<FinancialReportConsolidateDaily>, EntityRepository<FinancialReportConsolidateDaily>>(
+                s => new EntityRepository<FinancialReportConsolidateDaily>(connectionString, "cashflow_report", nameof(FinancialReportConsolidateDaily)));
+            return services;
+        }
+    }
+}
---
+++ src/CashFlow/CashFlow.CD.Api/model/FinancialReportStoreStock.cs
@@ -0,0 +1,8 @@
+﻿namespace CashFlow.CD.Api.model
+{
+    public class FinancialReportStoreStock
+    {
+        public decimal Amount { get; set; }
+        public int StoreId { get; set; }
+    }
+}
